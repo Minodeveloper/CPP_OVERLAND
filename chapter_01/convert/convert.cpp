840431c53e6459f0f4b470a45e84988317884e98
MADHURESH <minoshideveloper8@gmail.com>
2023-04-13 11:52:37 +0530

    vs code project

---
+++ chapter_01/convert/convert.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double	fahrenheit;
+	double	celcius;
+
+	cout << "Enter temperature in celcius: " << endl;
+	cin >> celcius;
+	fahrenheit = (celcius * 1.8) + 32;
+
+	cout << endl;
+	cout << "CELCIUS " << celcius << " " << "FAHRENHEIT " << fahrenheit << endl;
+
+
+	return 0;
+}
