3cb21392f34b6160dcb79e35cd302845620962ff
MADHURESH <minoshideveloper8@gmail.com>
2023-04-17 20:50:43 +0530

    random game

--- chapter_03/random/random.cpp
+++ chapter_03/random/random.cpp
@@ -1,20 +1,34 @@
-// random.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// random game guesser
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(nullptr));
+    int random = (rand() % 50) + 1;
+    int number;
+    bool game = true;
+    cout << "Guess the numer:" << endl;
+    do
+    {
+        cin >> number;
+        if (random < number) {
+            cout << "Too high!" << endl;
+        }
+        if (random > number) {
+            cout << "Too Low!" << endl;
+        }
+        if (number == random) {
+            cout << "WOW! You win." << endl;
+            game = false;
+        }
+
+    } while (game);
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
