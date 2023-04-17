6a6ef4294ca6c92f1ebb656f2b8bfdaf5ca2aac3
MADHURESH <minoshideveloper8@gmail.com>
2023-04-17 23:11:10 +0530

    addded avg and prime function call

--- chapter_05/functionCall/functionCall.cpp
+++ chapter_05/functionCall/functionCall.cpp
@@ -1,13 +1,24 @@
 // functionCall
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 void displayAverage(double num1, double num2); 
+void isPrime(int number);
 
 int main()
 {
-    displayAverage(34, 45);
+    int num1, num2;
+    cout << "Enter num1 ";
+    cin >> num1;
+    cout << "Enter num2 ";
+    cin >> num2;
+    cout << endl;
+    cout << "---------------------------------" << endl;
+    displayAverage(double(num1),double(num2));
+    isPrime(num1);
+    isPrime(num2);
+    
 
     return 0;
 }
@@ -18,3 +29,22 @@ void displayAverage(double num1, double num2)
     cout << "average of " << num1 << " and " << num2 << " is " << double((num1 + num2) / 2) << endl;
 }
 
+void isPrime(int number) {
+    bool prime = true;
+    for (size_t i = 2; i <= sqrt(number); i++)
+    {
+        if (number % i == 0) {
+            prime = false;
+            break;
+        }
+      
+    }
+
+    if (prime) {
+        cout << number << " is prime." << endl;
+    }
+    else {
+        cout << number << " is not prime." << endl;
+    }
+
+}
